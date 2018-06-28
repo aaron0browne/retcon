bf7e537a453055fe61f3a5e69d30b53e1d024443
Aaron Browne <aaron0browne@gmail.com>
2018-06-28 15:57:27 -0400

    Add browserpass install and completions
    Signed-off-by: Aaron Browne <aaron0browne@gmail.com>

---
+++ roles/common/files/zsh/completions.zsh
@@ -0,0 +1,6 @@
+# The following lines were added by compinstall
+zstyle :compinstall filename '/Users/brownea/.zsh/completions.zsh'
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
